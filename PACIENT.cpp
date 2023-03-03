44da61589d6081ea70ca53378fa1d62413364e05
JhonPava <jhonfernan2015@gmail.com>
2023-03-02 23:46:49 -0500

    MODIFICACION

--- PACIENT.cpp
+++ PACIENT.cpp
@@ -1,59 +1,106 @@
 #include <iostream>
-#include <queue>
 using namespace std;
-#define CANTIDAD_PACIENTES 100
 
 struct Paciente {
-  string nombre;
-  int documento;
+    string nombre;
+    int documento;
 };
 
+struct Nodo {
+    Paciente paciente;
+    Nodo* siguiente;
+};
 
-struct Paciente pt[CANTIDAD_PACIENTES];
+class PacientesCola {
+private:
+    Nodo* cabeza;
+    Nodo* cola;
+public:
+    PacientesCola() {
+        cabeza = NULL;
+        cola = NULL;
+    }
+
+    void agregarPaciente() {
+        Paciente p;
+        cout << "\nNombre del paciente -> ";
+        cin >> p.nombre;
+        cout << "\nNumero de documento ->  ";
+        cin >> p.documento;
+
+        Nodo* nuevoNodo = new Nodo;
+        nuevoNodo->paciente = p;
+        nuevoNodo->siguiente = NULL;
+
+        if (cabeza == NULL) {
+            cabeza = nuevoNodo;
+        } else {
+            cola->siguiente = nuevoNodo;
+        }
+        cola = nuevoNodo;
+
+        cout << "\nEl paciente ha sido registrado exitosamente." << endl;
+    }
+    
+    
+    
+    /Para mostrar los pacientes, recorremos la cola desde la cabeza y mostramos los datos de cada paciente./
+    void mostrarPacientes() {
+        if (cabeza == NULL) {
+            cout << "\nNo hay pacientes registrados." << endl;
+        } else {
+            int contador = 1;
+            cout << "\nPacientes registrados:" << endl;
+            Nodo* actual = cabeza;
+            while (actual != NULL) {
+                Paciente p = actual->paciente;
+                cout << contador << ". " << p.nombre << " - " << p.documento << endl;
+                actual = actual->siguiente;
+                contador++;
+            }
+        }
+    }
+    
+    /En el destructor, liberamos la memoria de todos los nodos antes de que la instancia de la clase sea destruida/
+    ~PacientesCola() {
+        Nodo* actual = cabeza;
+        while (actual != NULL) {
+            Nodo* temp = actual;
+            actual = actual->siguiente;
+            delete temp;
+        }
+    }
+};
+
 int main() {
-  
-  queue<Paciente> cola_pacientes;
+    PacientesCola cola;
 
-  
-  
-  int N;
-  cout<< "Ingrese el numero de pacientes a registrar "<<endl;
-  cin>>N;
+    int opcion;
+    do {
+        cout << "\nAdd and List (colas) pacientes" << endl;
+        cout << "1) Registrar Paciente A La Cola" << endl;
+        cout << "2) Mostrar Pacientes De La Cola" << endl;
+        cout << "Su opcion :  ";
+        cin >> opcion;
 
-  string nombre;
-  int documento;
+        switch (opcion) {
+            case 1: {
+                cola.agregarPaciente();
+                break;
+            }
+            case 2: {
+                cola.mostrarPacientes();
+                break;
+            }
+            case 0: {
+                break;
+            }
+            default: {
+                cout << "\n Opcion Incorrecta " << endl;
+                break;
+            }
+        }
+    } while (opcion != 0);
 
-  for (size_t i = 0; i < N ;i++)
-  {
-    cout <<"Ingresa el nombre del paciente "<<endl;
-    cin.ignore();
-    getline(cin,nombre);
-    pt[i].nombre=nombre;
-    cout<<"Ingresa el documento del paciente"<<endl;
-   	fflush(stdout);
-    cin>>documento;
-    pt[i].documento=documento;
-
-    cola_pacientes.push(pt[i]);
-  }
-
-  
-  int contador = 1;
-  while (!cola_pacientes.empty()) {
-    
-    Paciente p = cola_pacientes.front();
-
-    
-    cout << contador << ". ";
-    cout << "Nombre: " << p.nombre << ", ";
-    cout << "Documento: " << p.documento << ", " << endl;
-
-    
-    cola_pacientes.pop();
-
-    
-    contador++;
-  }
-
-  return 0;
+    return 0;
 }
